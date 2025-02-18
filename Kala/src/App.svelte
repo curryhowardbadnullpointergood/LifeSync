3d0944b524b80e60bc4f04cb271d7f1a34742ff6
Ash <darkovl10@gmail.com>
2025-02-18 13:48:43 +0000

    fixed tag error and started with the form

--- Kala/src/App.svelte
+++ Kala/src/App.svelte
@@ -1,10 +1,14 @@
+<svelte:options tag="my-app"/>
 
 <script>
     // It can be empty for this example
 </script>
-<svelte:options tag="my-app"/>
+
 <p>My App is working!</p> 
-<button> Submit</button>
+<button> Submit Task 1</button>
+<form> 
+    <button> Hello</button>
+</form>
 <style>
     /* It can be empty :) */
 </style>
