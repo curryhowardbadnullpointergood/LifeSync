594d360d6e125e361d04b711031ab72cf926de70
Ash <darkovl10@gmail.com>
2025-02-18 01:09:48 +0000

    set up go server and linked it to Svelte

---
+++ Kala/src/App.svelte
@@ -0,0 +1,12 @@
+
+<script>
+    // It can be empty for this example
+</script>
+<svelte:options tag="my-app"/>
+<p>My App is working!</p> 
+<button> Submit</button>
+<style>
+    /* It can be empty :) */
+</style>
+
+
