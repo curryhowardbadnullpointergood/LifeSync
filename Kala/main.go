095ccb294bf662ffd8d4f42e1023b0d507214603
Ash <darkovl10@gmail.com>
2025-03-06 17:21:50 +0000

    connected this to the sqlite3 database and got the tasks to update on the database

--- Kala/main.go
+++ Kala/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"embed"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"html/template"
 	"log"
 	"net/http"
 	"strings"
-    "database/sql"
-
-	"github.com/mattn/go-sqlite3"
 )
 
 //go:embed views/*
@@ -17,42 +16,40 @@ var views embed.FS
 
 var t = template.Must(template.ParseFS(views, "views/*"))
 
-
-
 // struct to represent the tasks
 type Tasks struct {
-    ID int
-    Task string 
+	ID   int
+	Task string
 }
 
-
-
-
-// Db is a global var for the sqlite database connection 
-var DB *sql.DB 
+// Db is a global var for the sqlite database connection
+var DB *sql.DB
 
 func initDB() {
-    var err error 
-    DB, err = sql.Open("sqlite3", "./app.db")
-    if err != nil { 
-        log.Fatal(err)
-    }
+	var err error
+	DB, err = sql.Open("sqlite3", "./app.db")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // SQL statement to create the todos table if it doesn't exist
-     sqlStmt := `
+	// SQL statement to create the todos table if it doesn't exist
+	sqlStmt := `
      CREATE TABLE IF NOT EXISTS todos (
       id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-      title TEXT
+      task TEXT
      );`
 
-     _, err = DB.Exec(sqlStmt)
-     if err != nil {
-      log.Fatalf("Error creating table: %q: %s\n", err, sqlStmt) // Log an error if table creation fails
-     }
+	_, err = DB.Exec(sqlStmt)
+	if err != nil {
+		log.Fatalf("Error creating table: %q: %s\n", err, sqlStmt) // Log an error if table creation fails
+	}
 
 }
 
 func main() {
+
+	initDB()
+	defer DB.Close()
 	router := http.NewServeMux()
 
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
@@ -64,7 +61,7 @@ func main() {
 	// Handle add task form submission
 	router.HandleFunc("POST /addtask", func(w http.ResponseWriter, r *http.Request) {
 
-		log.Println("Hello")
+		// log.Println("Hello")
 		if err := r.ParseForm(); err != nil {
 			http.Error(w, "Unable to parse task form: ", http.StatusInternalServerError)
 			return
@@ -73,12 +70,17 @@ func main() {
 		task := strings.ToLower(r.FormValue("taskInfo"))
 
 		log.Printf("Task to add: %s", task)
-        // initiates the database 
-        initDB()
-        // insert task into the database 
-        _,err := 
 
+		// insert task into the database
+		_, err := DB.Exec("INSERT INTO todos VALUES(NULL,?)", task)
 
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError) // Return an HTTP 500 error if insertion fails
+			return
+		}
+
+		// redirect to the main page after a sucessful creation of the task?
+		// http.Redirect(w, r, "/", http.StatusSeeOther)
 
 	})
 
