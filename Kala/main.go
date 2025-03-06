b02f651e8dd3330606f9e4b20fc9e82beb4dbf7a
Ash <darkovl10@gmail.com>
2025-03-06 16:50:57 +0000

    form

--- Kala/main.go
+++ Kala/main.go
@@ -7,12 +7,50 @@ import (
 	"log"
 	"net/http"
 	"strings"
+    "database/sql"
+
+	"github.com/mattn/go-sqlite3"
 )
 
 //go:embed views/*
 var views embed.FS
 
 var t = template.Must(template.ParseFS(views, "views/*"))
+
+
+
+// struct to represent the tasks
+type Tasks struct {
+    ID int
+    Task string 
+}
+
+
+
+
+// Db is a global var for the sqlite database connection 
+var DB *sql.DB 
+
+func initDB() {
+    var err error 
+    DB, err = sql.Open("sqlite3", "./app.db")
+    if err != nil { 
+        log.Fatal(err)
+    }
+
+    // SQL statement to create the todos table if it doesn't exist
+     sqlStmt := `
+     CREATE TABLE IF NOT EXISTS todos (
+      id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+      title TEXT
+     );`
+
+     _, err = DB.Exec(sqlStmt)
+     if err != nil {
+      log.Fatalf("Error creating table: %q: %s\n", err, sqlStmt) // Log an error if table creation fails
+     }
+
+}
 
 func main() {
 	router := http.NewServeMux()
@@ -35,6 +73,12 @@ func main() {
 		task := strings.ToLower(r.FormValue("taskInfo"))
 
 		log.Printf("Task to add: %s", task)
+        // initiates the database 
+        initDB()
+        // insert task into the database 
+        _,err := 
+
+
 
 	})
 
