961780c0575bbe59955d7527c7373ca695d3a0e6
curryhowardbadnullpointergood <darkovl10@gmail.com>
2025-11-08 20:08:57 +0000

    added some styling to the add tasks div

--- Kala/kala/src/routes/tasks/+page.svelte
+++ Kala/kala/src/routes/tasks/+page.svelte
@@ -20,7 +20,8 @@ let isDropdownOpen = false;
 
 // handles the add task temp div 
 let showTempTask = false;
-
+// handles the title input for the add new tasks temp div 
+let titleInput;
 
   const handleDropdownClick = () => {
     isDropdownOpen = !isDropdownOpen // togle state on click
@@ -84,8 +85,20 @@ let showTempTask = false;
 
     <div class="pendingtasks"> 
         {#if showTempTask}
-         <div class="tempaddclass"> 
-             Temp Add task test!
+         <div class="tempaddclass">
+
+            <div class="top"> 
+              <button> 
+<svg xmlns  ="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Z"/></svg>
+
+              </button>
+
+                  
+              <input type="text" placeholder="Title">
+
+            </div>
+
+             <p> Temp Add task test! </p>
          </div> 
         {/if}
 
