fad0773e458dbfff0f27afa8d080aa2dcb7a5a94
curryhowardbadnullpointergood <darkovl10@gmail.com>
2025-11-20 12:43:09 +0000

    tasks front end completed, well except range

--- Kala/kala/src/routes/tasks/+page.svelte
+++ Kala/kala/src/routes/tasks/+page.svelte
@@ -91,9 +91,6 @@ let showDateModal = false;
       }
     }
   onMount(() => {
-    const initial = repeatNumVal.dataset.initialValue;
-    repeatNumVal.value = initial;
-
     if (typeof document !== 'undefined') {
       document.addEventListener('click', handleClickOutside);
     }
@@ -272,6 +269,34 @@ let showDateModal = false;
 
                             <p>Never</p>
 
+    
+                        </div>
+                        <div class="on">
+                
+                            <button> 
+<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Z"/></svg>
+
+                            </button>
+
+                            <p>On</p>
+
+                            <input type="date" >
+
+    
+                        </div>
+                        <div class="after">
+                
+                            <button> 
+<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Z"/></svg>
+
+                            </button>
+
+                            <p>After</p>
+
+                            <div class="occurrences">
+                                <input type="number" value="1">
+                                <p>Occurrences</p>
+                            </div>
     
                         </div>
 
