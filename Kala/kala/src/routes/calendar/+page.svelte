f512b31aeba59f643a682e3eda1e4a58d4c669c8
curryhowardbadnullpointergood <darkovl10@gmail.com>
2026-02-24 23:41:45 +0000

    started on the calendar

---
+++ Kala/kala/src/routes/calendar/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+
+import "./calendar.scss"
+import Navbar from '../../lib/components/Navbar.svelte';
+import Sidebar from '../../lib/components/Sidebar.svelte';
+
+let isSidebarOpen = false
+
+</script>
+
+
+<div class="main">
+  <Navbar on:toggleSidebar={() => isSidebarOpen = !isSidebarOpen}/>
+  <Sidebar open={isSidebarOpen} />
+
+    <div class="block">
+
+    </div>
+  
+    
+
+
+
+</div>
