0132e090301a572f337c8d728f6c0ab48588b3b1
curryhowardbadnullpointergood <darkovl10@gmail.com>
2026-05-23 14:03:09 +0100

    timer

---
+++ Kala/kala/src/routes/time/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+
+import "./time.scss"
+import Navbar from '../../lib/components/Navbar.svelte';
+import Sidebar from '../../lib/components/Sidebar.svelte';
+
+let isSidebarOpen = false
+
+</script>
+
+
+<div class="main">
+  <Navbar on:toggleSidebar={() => isSidebarOpen = !isSidebarOpen}/>
+  <Sidebar open={isSidebarOpen} />
+
+    <div class="timer">
+    
+        <div class="container">
+            This is where you can adjust time. 
+
+        </div>
+    
+    </div>
+
+
+</div>
+
