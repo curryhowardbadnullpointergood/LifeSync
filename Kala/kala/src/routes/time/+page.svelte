23bd5cf4a4972c3fc8eed6236e9780400aa4473c
curryhowardbadnullpointergood <darkovl10@gmail.com>
2026-05-23 14:08:23 +0100

    semi working

--- Kala/kala/src/routes/time/+page.svelte
+++ Kala/kala/src/routes/time/+page.svelte
@@ -17,8 +17,17 @@ let isSidebarOpen = false
     
         <div class="container">
             This is where you can adjust time. 
-
+              <input type="number" id="hInput" value="0" min="0" style="width: 40px;">h
+              <input type="number" id="mInput" value="5" min="0" max="59" style="width: 40px;">m
+              <input type="number" id="sInput" value="0" min="0" max="59" style="width: 40px;">s
+              
+              <button id="startBtn">Start</button>
+              
+              <!-- Text Display -->
+              <h1 id="display">00:05:00</h1>
+        
         </div>
+
     
     </div>
 
